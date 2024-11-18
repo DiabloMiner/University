efe34f3f6b675ed9a53e91c593979284b179d6d5
DiabloMiner <martinrussler3@gmail.com>
2024-11-18 12:24:42 +0100

    Decided to use simpler solution for exercise 5

--- 1. Semester/EP1/HÜ/Aufgabenblatt3/src/Aufgabe5.java
+++ 1. Semester/EP1/HÜ/Aufgabenblatt3/src/Aufgabe5.java
@@ -4,42 +4,15 @@
 public class Aufgabe5 {
 
     private static String orderCharGroups(String text) {
-        // Base cases
-        if (text.length() <= 1) {
-            return text;
-        } else if (text.length() == 2) {
-            return text.charAt(1) < text.charAt(0) ? text.substring(1, 2) + text.charAt(0) : text;
-        }
-
-        // Recursive case
-        String sortedString;
-
-        // Check if first char should be swapped, then separate the first char of the new string
-        // and sort the rest of the input string, then add them up again
-        if (text.charAt(0) <= text.charAt(1)) {
-            sortedString = text.charAt(0) + orderCharGroups(text.substring(1));
-        } else {
-            sortedString = text.charAt(1) + orderCharGroups(text.charAt(0) + text.substring(2));
-        }
-
-        // Check if first character combined with the input string is already sorted
-        // or if the first char needs to be swapped and the rest of the string resorted
-        if (sortedString.charAt(0) > sortedString.charAt(1)) {
-            sortedString = sortedString.charAt(1) + orderCharGroups(sortedString.charAt(0) + sortedString.substring(2));
-        }
-        return sortedString;
-    }
-
-    private static String order(String text) {
         // Base case: (There is nothing to sort)
         if (text.length() <= 1) {
             return text;
         }
 
         // Recursive case: Split the first char from text, sort the remaining string,
-        // if the char is equal to the first char of the now sorted string add it to the front otherwise to the back
+        // If the char is equal to the first char of the now sorted string add it to the front otherwise to the back
         char firstChar = text.charAt(0);
-        String sortedString = order(text.substring(1));
+        String sortedString = orderCharGroups(text.substring(1));
         return (sortedString.charAt(0) == firstChar) ? firstChar + sortedString : sortedString + firstChar;
     }
 
