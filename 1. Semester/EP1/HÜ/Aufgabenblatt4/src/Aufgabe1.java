e19bad5e4af7e37d445c6bdb7495fc6a4f81d849
DiabloMiner <martinrussler3@gmail.com>
2024-11-20 14:18:33 +0100

    Solved exercise 1 and 2

--- 1. Semester/EP1/HÜ/Aufgabenblatt4/src/Aufgabe1.java
+++ 1. Semester/EP1/HÜ/Aufgabenblatt4/src/Aufgabe1.java
@@ -1,11 +1,13 @@
+import java.util.Arrays;
+
 /*
     Aufgabe 1) Code Analyse - Eindimensionale Arrays
 */
 public class Aufgabe1 {
 
     private static void printArray(int[] workArray) {
-        for (int i = workArray.length; i >= 0; i--) {
-            System.out.print(workArray[i-1] + " ");
+        for (int i = workArray.length; i >= 1; i--) {
+            System.out.print(workArray[i - 1] + " ");
         }
         System.out.println();
     }
@@ -58,11 +60,34 @@ public class Aufgabe1 {
     //**** Notizen und Fragebeantwortungen bitte hier unterhalb durchführen! ***
     //**************************************************************************
     //Antwort zu Punkt a:
-    //
+        // Es kommt bei [i - 1] zu IndexOutOfBoundsException da im for-Loop i >= 0 als Bedingung steht.
+
     //Antwort zu Punkt b:
-    //
+        // Die Methode fillArray hat keinen Rückgabewert, weil das als Input gegebene Array in der Methode modifiziert wird
+        // und dieses dann außerhalb von der Methode weiterverwendet wird.
+
     //Antwort zu Punkt c:
-    //
+        // Die "Kopie" ist eigentlich keine Kopie, sondern eine Referenz zum selben Objekt, das in der Methode modifiziert wird,
+        // wodurch das normale printArray dann natürlich auch das contentFilteredArray herausgibt.
+
     //Antwort zu Punkt d:
-    //
+        // Parameter in Java sind pass-by-Value, das heißt, dass der Wert des Parameters (in diesem Fall die Referenz auf das Array) kopiert wird,
+        // aber der Wert des Parameters kann nicht geändert werden, daher können wir mit der Referenz in printContentFilteredArray das eigentliche Array verändern,
+        // aber wir können nicht den Wert von workArray auf eine Referenz zu helpArray ändern.
+
+    // Zusatzfragen:
+    // 1:
+        // Er muss den primitiven Datentyp int haben.
+
+    // 2:
+        // Die Länge eines Arrays wird einmal gesetzt und ist ab dann unveränderbar.
+        // Es kann aber ein neues Array erstellt werden.
+
+    // 3:
+        // Man kann Arrays mit Arrays.copyOf() kopieren oder mit einem for-Loop und einem neuen int-Array.
+
+    // 4:
+        // Meist ist der Vergleich mit '==' nicht sinnvoll, da bei diesem Vergleich die Referenzen der beiden Objekte verglichen werden,
+        // das heißt es wird geprüft ob zwei Arrays das gleiche Objekt sind.
+
 }
--- 1. Semester/EP1/HÜ/Aufgabenblatt4/src/Aufgabe2.java
+++ 1. Semester/EP1/HÜ/Aufgabenblatt4/src/Aufgabe2.java
@@ -3,8 +3,74 @@
 */
 public class Aufgabe2 {
 
+    public static void printArray(short[] shortArray) {
+        String toBePrinted = "";
+        for (int i = 0; i < shortArray.length; i++) {
+            toBePrinted += (i == 0) ? "|" : "";
+            toBePrinted += shortArray[i];
+            toBePrinted += (i == shortArray.length - 1) ? "|" : ";";
+        }
+        System.out.println(toBePrinted);
+    }
+
+    public static void printArray(char[] charArray) {
+        String toBePrinted = "";
+        for (int i = 0; i < charArray.length; i++) {
+            toBePrinted += charArray[i];
+            toBePrinted += (i == charArray.length - 1) ? "" : " ";
+        }
+        System.out.println(toBePrinted);
+    }
+
+    public static void printArrayForLoop(int[] intArray) {
+        String toBePrinted = "for-Schleife: ";
+        for (int i = intArray.length - 1; i >= 0; i--) {
+            toBePrinted += intArray[i];
+            toBePrinted += (i == 0) ? "" : "!";
+        }
+        System.out.println(toBePrinted);
+    }
+
+    public static void printArrayWhileLoop(int[] intArray) {
+        String toBePrinted = "while-Schleife: ";
+        int i = intArray.length - 1;
+        while (i >= 0) {
+            toBePrinted += intArray[i];
+            toBePrinted += (i == 0) ? "" : "!";
+            i--;
+        }
+        System.out.println(toBePrinted);
+    }
+
     public static void main(String[] args) {
         // TODO: Implementieren Sie hier Ihre Lösung für die Angabe
+        short[] shortArray = new short[] {3, 6, 24, 31, 35, 44, 67, 89, 92};
+        printArray(shortArray);
+
+
+        char[] charArray = new char[] {'a', '8', '8', 'G', '2', ':', ':', ':', 'F', '7', 'b', 'b', '-', 'T'};
+        int insertions = 0, insertionsSoFar = 0;
+        for (int i = 0; i < charArray.length - 1; i++) {
+            insertions += (charArray[i] == charArray[i + 1]) ? 1 : 0;
+        }
+        char[] modifiedCharArray = new char[charArray.length + insertions];
+        for (int i = 0; i < modifiedCharArray.length; i++) {
+            modifiedCharArray[i] = charArray[i - insertionsSoFar];
+
+            if (i - insertionsSoFar != charArray.length - 1 && charArray[i - insertionsSoFar] == charArray[i -insertionsSoFar + 1]) {
+                modifiedCharArray[i + 1] = '+';
+                insertionsSoFar++;
+                i = i + 1;
+            }
+        }
+        printArray(modifiedCharArray);
+
+        int[] intArray = new int[20];
+        for (int i = 19; i >= 0; i--) {
+            intArray[(intArray.length - 1) - i] = i;
+        }
+        printArrayForLoop(intArray);
+        printArrayWhileLoop(intArray);
     }
 }
 
