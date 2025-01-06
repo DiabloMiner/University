9702b1f4ca0c9725988fcc378c36285786b875bc
DiabloMiner <martinrussler3@gmail.com>
2025-01-06 10:14:12 +0100

    Implemented visualization of minimax

--- 1. Semester/EP1/HÜ/Aufgabenblatt6/src/Aufgabe1.java
+++ 1. Semester/EP1/HÜ/Aufgabenblatt6/src/Aufgabe1.java
@@ -5,10 +5,13 @@
 import codedraw.*;
 
 public class Aufgabe1 {
-    public static void main(String[] args) {
 
-        int size = 600;
-        CodeDraw myDrawObj = new CodeDraw(size, size);
+    public static int size, maxDepth;
+    public static CodeDraw myDrawObj;
+
+    public static void main(String[] args) {
+        size = 600;
+        myDrawObj = new CodeDraw(size, size);
         myDrawObj.setTitle("Tic Tac Toe");
         EventScanner myEventSC = myDrawObj.getEventScanner();
 
@@ -20,8 +23,9 @@ public class Aufgabe1 {
 
         boolean twoPlayer = false; //true ... human vs. human, false ... human vs. computer
         boolean player = true; //(1Player) human = true, computer = false, (2Player) human1 = true, human2 = false
-        int maxDepth = 3;
+        maxDepth = 3;
         boolean gameRunning = true;
+        boolean showMinimax = false;
 
         drawGameBoard(myDrawObj, gameBoard);
 
@@ -39,7 +43,13 @@ public class Aufgabe1 {
                 player = !player;
             } else if (!twoPlayer && !player) {
                 // Call minimax to get indices for best move
-                int[] retArray = minimax(gameBoard, false, maxDepth);
+                int[] retArray;
+                if (showMinimax) {
+                    retArray = minimax(gameBoard, false, maxDepth, 0, size, 0, size);
+                    myDrawObj.show();
+                } else {
+                    retArray = minimax(gameBoard, false, maxDepth);
+                }
 
                 // Set gameboard at indices to computer symbol
                 gameBoard[retArray[0]][retArray[1]] = 'O';
@@ -59,7 +69,9 @@ public class Aufgabe1 {
                 gameRunning = false;
             }
 
+            myDrawObj.clear();
             drawGameBoard(myDrawObj, gameBoard);
+            myDrawObj.show();
         }
     }
 
@@ -96,8 +108,48 @@ public class Aufgabe1 {
         return retArray;
     }
 
+    private static int[] minimax(char[][] gameBoard, boolean player, int depth, double lastMinX, double lastMaxX, double lastMinY, double lastMaxY) {
+        int[] retArray = new int[3];
+        retArray[2] = player ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+        for (int i = 0; i < gameBoard.length; i++) {
+            for (int j = 0; j < gameBoard[i].length; j++) {
+                if (Character.isWhitespace(gameBoard[i][j])) {
+                    gameBoard[i][j] = player ? 'X' : 'O';
+
+                    // Compute minX, maxX, minY, maxY
+                    double width = lastMaxX - lastMinX, height = lastMaxY - lastMinY;
+                    double widthThird = width / 3, heightThird = height / 3;
+                    double minX = lastMinX + widthThird * j, maxX = lastMinX + widthThird * (j + 1), minY = lastMinY + heightThird * i, maxY = lastMinY + heightThird * (i + 1);
+
+                    if (checkIfWinner(gameBoard, true)) {
+                        retArray = new int[] {i, j, -1};
+                    } else if (checkIfWinner(gameBoard, false)) {
+                        retArray = new int[] {i, j, 1};
+                    } else if (depth - 1 == 0 || checkIfFull(gameBoard)) {
+                        retArray = new int[] {i, j, 0};
+                    } else {
+                        int[] tempArray = minimax(gameBoard, !player, depth - 1, minX, maxX, minY, maxY);
+                        if (player) {
+                            if (tempArray[2] < retArray[2]) {
+                                retArray = new int[] {i, j, tempArray[2]};
+                            }
+                        } else {
+                            if (tempArray[2] > retArray[2]) {
+                                retArray = new int[] {i, j, tempArray[2]};
+                            }
+                        }
+                    }
+
+                    drawGameBoard(myDrawObj, gameBoard, minX, maxX, minY, maxY);
+
+                    gameBoard[i][j] = ' ';
+                }
+            }
+        }
+        return retArray;
+    }
+
     private static boolean checkIfFull(char[][] gameBoard) {
-        // TODO: Implementieren Sie hier Ihre Lösung für die Methode
         for (char[] row : gameBoard) {
             for (char entry : row) {
                 if (entry == ' ') {
@@ -105,11 +157,10 @@ public class Aufgabe1 {
                 }
             }
         }
-        return true; //Zeile kann geändert oder entfernt werden.
+        return true;
     }
 
     private static boolean checkIfWinner(char[][] gameBoard, boolean player) {
-        // TODO: Implementieren Sie hier Ihre Lösung für die Methode
         char checkChar = player ? 'X' : 'O';
 
         for (int i = 0; i < gameBoard.length; i++) {
@@ -149,26 +200,24 @@ public class Aufgabe1 {
             }
         }
 
-        return false; //Zeile kann geändert oder entfernt werden.
+        return false;
     }
 
     private static void drawGameBoard(CodeDraw myDrawObj, char[][] gameBoard) {
-        // TODO: Implementieren Sie hier Ihre Lösung für die Methode
         // Draw four lines
-        int width = myDrawObj.getWidth(), height = myDrawObj.getHeight(), widthThird = width / 3, heightThird = width / 3;
+        int width = size, height = size, widthThird = width / 3, heightThird = width / 3;
 
-        myDrawObj.drawLine(0, widthThird, width, widthThird);
-        myDrawObj.drawLine(0, 2 * widthThird, width, 2 * widthThird);
+        myDrawObj.drawLine(0, heightThird, width, heightThird);
+        myDrawObj.drawLine(0, 2 * heightThird, width, 2 * heightThird);
         myDrawObj.drawLine(widthThird, 0, widthThird, height);
         myDrawObj.drawLine(2 * widthThird, 0, 2 * widthThird, height);
-
 
         // Draw entries of gameBoard
         for (int i = 0; i < gameBoard.length; i++) {
             for (int j = 0; j < gameBoard[i].length; j++) {
-                int minX = j * widthThird, maxX = (j + 1) * widthThird, minY = i * widthThird, maxY = (i + 1) * widthThird;
+                int minX = j * widthThird, maxX = (j + 1) * widthThird, minY = i * heightThird, maxY = (i + 1) * heightThird;
 
-                // Draw cross
+                // Draw X
                 if (gameBoard[i][j] == 'X') {
                     myDrawObj.drawLine(minX, minY, maxX, maxY);
                     myDrawObj.drawLine(minX, maxY, maxX, minY);
@@ -176,13 +225,42 @@ public class Aufgabe1 {
 
                 // Draw circle
                 if (gameBoard[i][j] == 'O') {
-                    myDrawObj.drawCircle((minX + maxX) / 2, (minY + maxY) / 2, widthThird / 2);
+                    myDrawObj.drawCircle((double) (minX + maxX) / 2, (double) (minY + maxY) / 2, (double) widthThird / 2);
                 }
 
             }
         }
+    }
 
-        myDrawObj.show();
+    private static void drawGameBoard(CodeDraw myDrawObj, char[][] gameBoard, double minX, double maxX, double minY, double maxY) {
+        // Draw four lines
+        double width = maxX - minX, height = maxY - minY;
+        double widthThird = width / 3, heightThird = height / 3;
+
+        myDrawObj.drawLine(minX, minY + heightThird, maxX, minY + heightThird);
+        myDrawObj.drawLine(minX, minY + 2 * heightThird, maxX, minY + 2 * heightThird);
+        myDrawObj.drawLine(minX + widthThird, minY, minX + widthThird, maxY);
+        myDrawObj.drawLine(minX + 2 * widthThird, minY, minX + 2 * widthThird, maxY);
+
+
+        // Draw entries of gameBoard
+        for (int i = 0; i < gameBoard.length; i++) {
+            for (int j = 0; j < gameBoard[i].length; j++) {
+                double minX2 = minX + j * widthThird, maxX2 = minX + (j + 1) * widthThird, minY2 = minY + i * heightThird, maxY2 = minY + (i + 1) * heightThird;
+
+                // Draw cross
+                if (gameBoard[i][j] == 'X') {
+                    myDrawObj.drawLine(minX2, minY2, maxX2, maxY2);
+                    myDrawObj.drawLine(minX2, maxY2, maxX2, minY2);
+                }
+
+                // Draw circle
+                if (gameBoard[i][j] == 'O') {
+                    myDrawObj.drawCircle((minX2 + maxX2) / 2, (minY2 + maxY2) / 2, widthThird / 2);
+                }
+
+            }
+        }
     }
 
 }
